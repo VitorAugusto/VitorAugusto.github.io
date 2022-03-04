c24dc52eeb7cfd29d29c283bcfaae03cdedcb63b
VitorAugusto <vitoraugusto2508@gmail.com>
2022-03-04 12:40:51 -0300

    arquivos

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+    $('#btn-prev').click(function() {
+        $('#carouselExampleIndicators').carousel('prev');
+      });
+      
+      $('#btn-next').click(function() {
+        $('#carouselExampleIndicators').carousel('next');
+      });
+      
+});
